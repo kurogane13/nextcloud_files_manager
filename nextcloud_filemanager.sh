8bb928cbe67fefdac7053c3dcb03d17d0603aefa
Gus <akaisuzuki@gmail.com>
2024-07-25 16:06:43 -0300

    Update nextcloud_filemanager.sh

--- nextcloud_filemanager.sh
+++ nextcloud_filemanager.sh
@@ -7,6 +7,30 @@ list_folder_shares() {
     main_menu
 }
 
+share_link() {
+
+    # Define the file path
+    share_link_file="share_link_file.txt"
+    echo
+    echo "Validating if the folder has a share link file..."
+	echo
+    # Check if the share link file already exists
+    if [[ -f "$folder_share/$share_link_file" ]]; then
+        share_link=$(cat "$share_link_file")
+        echo
+        echo "Validation OK!. This folder already has a share link file: $share_link_file"
+        echo
+    else
+        # Prompt for the share link URL
+        echo
+        read -p "No share link URL file was found for this folder: Provide your share link url, and press enter to proceed: " share_link
+
+        # Create the file and write the share link to it
+        echo "$share_link" > "$folder_share/$share_link_file"
+    fi
+	
+}
+
 download_content() {
     echo 
     ls -lha
@@ -14,48 +38,45 @@ download_content() {
     echo "---------------------------------------------------------------------"
     
     # Prompt user for input
-    read -p "Enter the folder share to download content to (e.g., nextcloud): " folder_share
-    echo
-    echo "You will be downloading content from nextcloud to folder: $folder_share"
+    read -p "Enter the folder share to download content to without '/' (e.g., nextcloud): " folder_share
+    if [[ ! -d $folder_share ]]; then
+		  echo
+		  echo "$folder_share not found in $PWD."
+		  echo
+		  read -p "Press enter to provide an existing folder and/or subfolder: " enter
+		  download_content
+    else
+          echo
+    fi
+    echo "You will be downloading content from nextcloud to local folder: $folder_share"
     echo
     echo "All the content will be downloaded in a zip file called nextcloud_download_content.zip with a datetime timestamp."
     echo
-    read -p "Press enter to proceed: " enter
-
-    # Define the file path
-    share_link_file="$folder_share/share_link_download.txt"
-
-    # Create the directory if it doesn't exist
-    mkdir -p "$(dirname "$share_link_file")"
-
-    # Check if the share link file already exists
-    if [[ -f "$share_link_file" ]]; then
-        share_link=$(cat "$share_link_file")
-        echo "This folder already has a download link: $share_link"
-        echo
-    else
-        # Prompt for the share link URL
-        read -p "Provide your share link url, to download all content to your $folder_share folder: " share_link
-
-        # Create the file and write the share link to it
-        echo "$share_link" > "$share_link_file"
-    fi
-
-    # Clean the share link URL and append /download if necessary
+    
+    # Get the share link URL
+    share_link
+    
+    echo "------------------------------------------------------------------------------------------"
+    echo
+    read -p "Press enter to proceed to download content to local share folder: $folder_share: "
+    
+    # Clean and prepare the share link URL
     download_url=$(echo "$share_link" | sed -r 's#(/download)+$#/download#')
-    download_url="${download_url}/download"
-
-    # Get the current datetime and format it for the filename
     datetimestamp=$(date +'%Y-%m-%d_%H-%M-%S')
     zip_filename="nextcloud_download_content_$datetimestamp.zip"
     zip_file="$folder_share/$zip_filename"
+    
+    # Construct the corrected URL for download
+    corrected_url="${download_url}/download/${zip_filename}"
+    corrected_url=$(echo "$corrected_url" | sed 's|/public.php/webdav//|/public.php/webdav/|g')
+    
     echo
-    echo "Downloading content from $download_url to $zip_file..."
+    echo "Downloading content from $corrected_url to $zip_file..."
     echo
     
     # Perform the download using curl
-    curl -L -v "$download_url" -o "$zip_file"
-
+    curl -L -v "$corrected_url" -o "$zip_file"
+    
     # Check if the download was successful
     if [[ $? -eq 0 ]]; then
         echo "--------------------------------------------------------------"
@@ -66,14 +87,21 @@ download_content() {
         
         # Unzip the file
         unzip "$zip_file" -d "$folder_share"
-        echo
-        echo "Listing all files in: $folder_share"
-        ls -lha "$folder_share"
-        echo "--------------------------------------------------------------"
+        
+        # Check if unzip was successful
+        if [[ $? -eq 0 ]]; then
+            echo
+            rm -rf $zip_file
+            echo "Listing all files in: $folder_share"
+            ls -lha "$folder_share"
+            echo "--------------------------------------------------------------"
+        else
+            echo "Unzip failed. The file may be corrupted."
+        fi
     else
         echo "Download failed. Please check the share link or your connection."
     fi
-
+	echo
     read -p "Press enter to get back to the main menu: " enter
     main_menu
 }
@@ -87,94 +115,150 @@ upload_content() {
     echo
     read -p "Choose an option (1 or 2): " choice
 
-    # Define the file path for the share link file
-    share_link_file="$folder_share/share_link_upload.txt"
-
-    if [[ $choice -eq 1 ]] || [[ $choice -eq 2 ]]; then
-        # Check if the share link file exists
-        if [[ -f "$share_link_file" ]]; then
-            share_link=$(cat "$share_link_file")
+    if [[ $choice -eq 1 ]]; then
+        # Ask for the full path to the file
+        echo
+        read -p "Enter the full path to the file (Example: My_folder/sub_folder/file.json): " filepath
+        filename=$(basename "$filepath")
+        
+        if [[ ! -f $filepath ]];then
             echo
-            echo "This folder already has an upload link: $share_link"
+            echo "ERROR: $filepath not found"
+            echo
+            read -p "Press enter to return to the main menu: "
+            echo
+            upload_content
         else
             echo
-            echo "No share link found in $folder_share"
-            # Prompt for the share link URL
-            echo
-            read -p "Provide your share link URL, to upload all content to your $folder_share folder: " share_link
-            # Create the file and write the share link to it
-            echo "$share_link" > "$share_link_file"
+			echo "Provided $filepath is valid"
+			echo 
+			ls -lha $filepath
         fi
 
-        # Get the share ID from the share link
+        # Define the file path for the share link file
+        share_link_file="$(dirname "$filepath")/share_link_upload.txt"
+
+        # Check if the share link file exists
+        if [[ ! -f "$share_link_file" ]]; then
+            echo
+            echo "Share link file not found. Please provide the share link."
+            echo
+            read -p "Provide the share link URL, to proceed with the oupload of $filepath: " share_link
+            echo "$share_link" > "$share_link_file"
+        else
+            share_link=$(<"$share_link_file")
+        fi
+
         share_id=$(echo "$share_link" | sed -n 's#https://nc.cloudlinux.com/s/\([^/]*\)$#\1#p')
+        echo
+        echo "-----------------------------------------------------------------------------------------"
+        echo
+        read -p "Press enter to proceed to the upload of $filepath to Cloudlinx nextcloud share: " enter
+        echo
 
-        if [[ $choice -eq 1 ]]; then
-            # Ask for the full path to the file
+        # Check if the file exists
+        if [[ -f "$filepath" ]]; then
+            # Zip the file
+            temp_zip="${filepath}.zip"
+            echo "Compressing $filepath to $temp_zip..."
+            zip -j "$temp_zip" "$filepath"
+			
+			corrected_url="https://nc.cloudlinux.com/public.php/webdav/$folder_share/$filename"
+            corrected_url2=$(echo "$corrected_url" | sed 's|/webdav//|/webdav/|g')
+            # Perform the upload using curl
             echo
-            read -p "Enter the full path to the file: " filepath
-            filename=$(basename "$filepath")
-            filedir=$(dirname "$filepath")
+            echo "Uploading $temp_zip to $share_link..."
+            curl -v -k -T "$temp_zip" -u "$share_id:" -H 'X-Requested-With: XMLHttpRequest' $corrected_url2
 
-            # Check if the file exists
-            if [[ -f "$filepath" ]]; then
-                # Construct the URL for the file
-                corrected_url="https://nc.cloudlinux.com/public.php/webdav/$folder_share/$filename"
-                corrected_url=$(echo "$corrected_url" | sed 's|/webdav//|/webdav/|g')
-
-                # Perform the upload using curl
+            if [[ $? -eq 0 ]]; then
                 echo
-                echo "Uploading $filename to $corrected_url..."
-                curl -v -k -T "$filepath" -u "$share_id:" -H 'X-Requested-With: XMLHttpRequest' "$corrected_url"
-
-                if [[ $? -eq 0 ]]; then
-                    echo
-                    echo "File $filename uploaded successfully."
-                else
-                    echo
-                    echo "Failed to upload file $filename."
-                fi
+                echo "File $filename uploaded successfully."
             else
                 echo
-                echo "File $filepath does not exist."
+                echo "Failed to upload file $filename."
             fi
-        elif [[ $choice -eq 2 ]]; then
-            # Ask for the full path to the folder
+
+            # Clean up the temporary zip file
+            rm -f "$temp_zip"
+        else
             echo
-            read -p "Enter the full path to the folder you want to upload to Nextcloud (Example: /root/My_folder): " folderpath
-            foldername=$(basename "$folderpath")
-            temp_zip="$folderpath.zip"
+            echo "File $filepath does not exist."
+            echo
+            read -p "Press enter to return to the menu: " enter
+            upload_content
+        fi
+    elif [[ $choice -eq 2 ]]; then
+        # Ask for the full path to the folder
+        echo
+        read -p "Enter the full path to the folder you want to upload to Nextcloud without the last '/' (Example: /root/My_folder): " folder_share
+        foldername=$(basename "$folder_share")
+        temp_zip="$folder_share.zip"
+        
+        if [[ ! -d $folder_share ]];then
+            echo
+            echo "ERROR: Folder share: $folder_share not found"
+            echo
+            read -p "Press enter to return to the main menu: "
+            echo
+            upload_content
+        else
+            echo
+			echo "Provided $folder_share is valid"
+			echo 
+			ls -lha $folder_share
+        fi
 
-            # Check if the folder exists
-            if [[ -d "$folderpath" ]]; then
-                # Compress the folder using zip
+        # Define the file path for the share link file
+        share_link_file="$(dirname "$folder_share")/share_link_upload.txt"
+
+        # Check if the share link file exists
+        if [[ ! -f "$share_link_file" ]]; then
+            echo
+            echo "Share link file not found. Please provide the share link."
+            echo
+            read -p "Provide the share link URL to proceed with the upload of $folder_share: " share_link
+            echo "$share_link" > "$share_link_file"
+        else
+            share_link=$(<"$share_link_file")
+        fi
+
+        share_id=$(echo "$share_link" | sed -n 's#https://nc.cloudlinux.com/s/\([^/]*\)$#\1#p')
+        
+        echo "-----------------------------------------------------------------------------------------"
+        echo
+        read -p "Press enter to proceed to the upload of $folder_share to Cloudlinx nextcloud share: " enter
+        echo
+
+        # Check if the folder exists
+        if [[ -d "$folder_share" ]]; then
+            # Compress the folder using zip
+            echo
+            echo "Compressing $folder_share to $temp_zip..."
+            zip -r "$temp_zip" "$folder_share"
+
+			corrected_url="https://nc.cloudlinux.com/public.php/webdav/$folder_share/$filename"
+            corrected_url2=$(echo "$corrected_url" | sed 's|/webdav//|/webdav/|g')
+            # Perform the upload using curl
+            echo
+            echo "Uploading $temp_zip to $share_link..."
+            curl -v -k -T "$temp_zip" -u "$share_id:" -H 'X-Requested-With: XMLHttpRequest' $corrected_url2
+
+            if [[ $? -eq 0 ]]; then
                 echo
-                echo "Compressing $folderpath to $temp_zip..."
-                zip -r "$temp_zip" "$folderpath"
-
-                # Construct the URL for the folder
-                corrected_url="https://nc.cloudlinux.com/public.php/webdav/$folder_share/$temp_zip"
-                corrected_url=$(echo "$corrected_url" | sed 's|/webdav//|/webdav/|g')
-
-                # Perform the upload using curl
-                echo
-                echo "Uploading $temp_zip to $corrected_url..."
-                curl -v -k -T "$temp_zip" -u "$share_id:" -H 'X-Requested-With: XMLHttpRequest' "$corrected_url"
-
-                if [[ $? -eq 0 ]]; then
-                    echo
-                    echo "Folder $foldername uploaded successfully."
-                else
-                    echo
-                    echo "Failed to upload folder $foldername."
-                fi
-
-                # Clean up the temporary zip file
-                rm "$temp_zip"
+                echo "Folder $foldername uploaded successfully."
             else
                 echo
-                echo "Folder $folderpath does not exist."
+                echo "Failed to upload folder $foldername."
             fi
+
+            # Clean up the temporary zip file
+            rm -f "$temp_zip"
+        else
+            echo
+            echo "Folder $folder_share does not exist."
+            echo
+            read -p "Press enter to return to the menu: " enter
+            upload_content
         fi
     else
         echo
@@ -189,7 +273,8 @@ main_menu() {
     while true; do
         clear
         echo "=============================="
-        echo " Nextcloud Shares File Manager "
+        echo " Nextcloud Shares File Manager "debug2: channel 0: window 999236 sent adjust 49340
+
         echo "=============================="
         echo "1. List all folders and files in $PWD"
         echo "2. Download all content from a Nextcloud share"
@@ -210,3 +295,4 @@ main_menu() {
 }
 
 main_menu
+
